e7600f6cfedc4fc1137e4c6c30a2ccfe4c859324
Masahiro Yamauchi <yamauchi@geishatokyo.com>
2015-02-06 16:22:13 +0900

    Change framework from bottle to flask
    Because uploading file failure

---
+++ web.py
@@ -0,0 +1,31 @@
+import os
+import sys
+from flask import Flask, request, render_template
+import cups
+import tempfile
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "hello"
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'GET':
+        return render_template('upload.html')
+    else:
+        conn = cups.Connection()
+        upload = request.files['upload']
+        with tempfile.NamedTemporaryFile() as fp:
+            upload.save(fp.name)
+            conn.printFile(app.config['printer_name'], fp.name, 'WebPrinter', {})
+        return upload.filename
+
+if __name__ == '__main__':
+    args = sys.argv
+    port = int(args[1])
+    printer_name = args[2]
+    app.config['printer_name'] = printer_name
+    app.run(host='0.0.0.0', port=port, debug=True)
+
