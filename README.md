ce2f04fd3e93ccac55a145364fb8c902273f9049
Masahiro Yamauchi <yamauchi@geishatokyo.com>
2015-02-06 16:31:58 +0900

    Merge

--- README.md
+++ README.md
@@ -2,3 +2,18 @@
 
 Printing web application with cups
 
+## Dependencies
+
+* Linux
+* Python 3
+* Flask
+* Cups
+* Pycups
+
+## Usage
+
+First install python 3 on your linux.
+```
+pip install -r requirements.txt
+python web.py 5000 <PRINTER_NAME>
+```
--- web.py
+++ web.py
@@ -20,12 +20,12 @@ def upload():
         with tempfile.NamedTemporaryFile() as fp:
             upload.save(fp.name)
             conn.printFile(app.config['printer_name'], fp.name, 'WebPrinter', {})
-        return upload.filename
+        return '<p>Now printing %s. Go to the printer!</p>' % upload.filename
 
 if __name__ == '__main__':
     args = sys.argv
     port = int(args[1])
     printer_name = args[2]
     app.config['printer_name'] = printer_name
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
 
